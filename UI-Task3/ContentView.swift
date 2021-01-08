e3bafd1e4f17c9149b3ec3c2e6954e83042bb79a
Zekhniddin <zexjumayev@gmail.com>
2021-01-08 21:46:55 +0500

    Initial Commit

---
+++ UI-Task3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI-Task3
+//
+//  Created by Зехниддин on 08/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
