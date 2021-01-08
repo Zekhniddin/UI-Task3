333ec3c52d3db74663ebcb54aa94fcd87acb4fc8
Zekhniddin <zexjumayev@gmail.com>
2021-01-08 22:05:36 +0500

    .

--- UI-Task3/ContentView.swift
+++ UI-Task3/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.gray
+                .opacity(0.4)
+                .edgesIgnoringSafeArea(.all)
+            TabView {
+                ImagePageTabView(name: "photo1")
+                ImagePageTabView(name: "photo2")
+                ImagePageTabView(name: "photo3")
+                ImagePageTabView(name: "photo4")
+                ImagePageTabView(name: "photo5")
+            }
+            .tabViewStyle(PageTabViewStyle())
+        }
+    }
+}
+
+struct ImagePageTabView: View {
+    var name: String
+    
+    var body: some View {
+        Image(name)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: 100, maxHeight: .infinity)
+            .clipped()
+            .shadow(color: .gray, radius: 10)
     }
 }
 
@@ -19,3 +44,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
